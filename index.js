99236bedba5f79b7e993271473efcb236d1e0701
F. T. Costa <47283591+CasualCosta@users.noreply.github.com>
2024-05-27 14:22:49 -0300

    Get update

--- index.js
+++ index.js
@@ -10,15 +10,45 @@ app.get('/', (req, res) => {
     res.send("Hello from Node API updated")
 })
 
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findById(id)
+
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+
 app.post('/api/products', async (req, res) => {
     try {
         const product = await Product.create(req.body)
         res.status(200).json(product)
     } catch (error) {
         res.status(500).json({message: error.message})
-        // console.log(error)
     }
 })
+
+
+app.put('/api/product/:id', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 
 mongoose.connect('mongodb+srv://Felipe:x6sYBSCxperaPEKK@backenddb.kn98cht.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB')
 .then(() => {

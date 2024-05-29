8efdf66b11eb4e45d455bb66d0f71eb966fc74a6
F. T. Costa <47283591+CasualCosta@users.noreply.github.com>
2024-05-29 20:15:09 -0300

    Finished

--- index.js
+++ index.js
@@ -1,52 +1,15 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const Product = require("./models/product.model")
+const productRoute = require("./routes/product.route")
 const app = express()
 
-
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use("/api/products", productRoute)
 
 app.get('/', (req, res) => {
     res.send("Hello from Node API updated")
-})
-
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({})
-        res.status(200).json(products)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-app.get('/api/product/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        const product = await Product.findById(id)
-
-        res.status(200).json(product)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(200).json(product)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-
-app.put('/api/product/:id', async (req, res) => {
-    try {
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
 })
 
 
@@ -54,7 +17,7 @@ mongoose.connect('mongodb+srv://Felipe:x6sYBSCxperaPEKK@backenddb.kn98cht.mongod
 .then(() => {
     console.log("Connected to database")
     app.listen(3000, () => {
-        console.log("Server running on port 3000")
+        console.log("Server running on port 3000. Access through: http://localhost:3000/")
     })
 })
 .catch(() => {
